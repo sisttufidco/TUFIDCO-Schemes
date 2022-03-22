d8f82aa8af07754b07ac5ae986cea2a3ab7e780f
aryanbhatt1 <aryanbhatt1002@gmail.com>
2022-03-22 17:49:52 +0530

    jh

--- CTP/apps.py
+++ CTP/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class CtpConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'CTP'
+    verbose_name = 'Town Panchayats'
--- DMA/apps.py
+++ DMA/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DmaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'DMA'
+    verbose_name = 'Municipalities'
